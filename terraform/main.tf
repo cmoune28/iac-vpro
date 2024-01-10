7d7b8b40f40f793c466065c0871ebf0c9c34bc8d
Ced M <mouneac@gmail.com>
2024-01-10 17:55:25 -0600

    Testing workflows #7

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "kubernetes" {
 provider "aws" {
   region = var.region
   shared_credentials_files = [ "/Users/ced_m/.aws/credentials" ]
-  profile = var.AwsProfile
+  # profile = var.AwsProfile
 }
 
 data "aws_availability_zones" "available" {}
